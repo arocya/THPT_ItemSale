af9fb1bba6a5df03e2fcfbec0b2854649bef0962
arocya <arion.almond@gmail.com>
2016-03-15 22:38:27 -0400

    InitialCommit

---
+++ DriverClass.java
@@ -0,0 +1,80 @@
+package THPT_ItemSale;
+
+
+/**
+ * This is the driver class for SaleItem.
+ * 
+ * @author Arion Almond 
+ * @version 2/19/2014
+ */
+
+import java.util.*;
+public class DriverClass
+{
+        public static void main (String[]args)
+        {
+            Scanner input = new Scanner(System.in);
+            
+            SaleItem itemCost = new SaleItem();
+            
+            double inItemPrice = 0;
+            double inCashAmount = 0;
+            double discountPrice = 0;
+            double totalPrice = 0;
+            double inDiscountPercent =0;
+            char repeat = 'y'; 
+            
+            do
+            {
+            System.out.println("Welcome to the Item Cost Calculator. We will calculate \nhow much your item " +
+                               "was discounted.  \nStandard discount is 10 percent!"  +
+                               "\nPlease enter the price of the item : ");
+                               
+            inItemPrice = input.nextDouble();
+            itemCost.setPrice(inItemPrice);
+            
+            System.out.println("How much cash would you like to pay with? \n" +
+                               "Please enter the amount of cash : " );
+                              
+            inCashAmount = input.nextDouble();
+            itemCost.setCash(inCashAmount);
+            
+            discountPrice = itemCost.getDiscountPrice();
+            totalPrice = itemCost.getFinalPrice();
+            
+            
+            
+            System.out.println("Results: \n Original Price : " + inItemPrice +
+                                "\n Discount Price : " + discountPrice +
+                                "\n Total Price (tax included) : " + totalPrice +
+                                "\n Change Due: " + (inCashAmount-totalPrice));
+                                
+                                
+            System.out.println("What would you like to change the discount percent to: ");
+            
+            inDiscountPercent = input.nextDouble();
+            itemCost.setDiscountPercent(inDiscountPercent);
+            
+            System.out.println("How much cash would you like to pay with? \n" +
+                               "Please enter the amount of cash : " );
+                              
+            inCashAmount = input.nextDouble();
+            itemCost.setCash(inCashAmount);
+            discountPrice = itemCost.getDiscountPrice();
+            totalPrice = itemCost.getFinalPrice();
+                              
+            System.out.println("Results: \n Original Price : " + inItemPrice +
+                        "\n Discount Price : " + discountPrice +
+                        "\n Total Price (tax included) : " + totalPrice +
+                        "\n Change Due: " + (inCashAmount-totalPrice));
+            
+            
+            System.out.println("Would you like to find the discount price of another item? " +
+                                "\n Enter (y) for yes or (n) for no: ");
+                
+            repeat = input.next().charAt(0);
+            
+        }while(repeat =='y');
+    
+    }
+}
---
+++ SaleItem.java
@@ -0,0 +1,133 @@
+package THPT_ItemSale;
+
+
+/**
+ * SaleItem lets users input the price of items and set a discount rate
+ * that will reduce the total price.  Also a user will input the amount of money
+ * given and the change recieved will be calculated.  
+ * 
+ * @author Arion Almond
+ * @version 2/19/2014
+ */
+public class SaleItem
+{
+    
+    
+    double priceOfItem;
+    double discountPercent;
+    double taxRate;
+    double cash;
+    double discountPrice;
+    double finalPrice;
+    double change;
+    
+    /**
+     * no-args constructor that that sets values for fields
+     */
+    
+    public SaleItem()
+    {
+        priceOfItem = 0;
+        discountPercent = .10;
+        taxRate = .075;
+        cash = 0;
+        
+        
+    }
+    
+    
+    /**
+     * This constructor has arguments to allow the user to set the
+     * initial values.  
+     * @param double - the value for priceOfItem
+     * @param double - the value for discountPercent
+     * @param double - the value for the taxRate
+     */
+    
+    public SaleItem(double price, double discount, double tax)
+    {
+        priceOfItem = price;
+        discountPercent = discount;
+        taxRate = tax;
+    }
+    
+    
+    /**
+     * This method is used to set the price of the item.
+     * @param double - the changed value of priceOfItem
+     * 
+     */
+    
+    public void setPrice(double price)
+    {
+        priceOfItem = price;
+    }
+    
+    /**
+     * This method is used to set the discount percent.
+     * @param double - the changed value of the discountPercent
+     */
+    
+    public void setDiscountPercent(double discount)
+    {
+        discountPercent = discount;
+    }
+    
+    
+   /**
+     * This method is used to set the tax rate.
+     * @param double - the changed value of the taxRate
+     */
+    
+    public void setTaxRate(double tax)
+    {
+        taxRate = tax;
+    }
+    
+     /**
+     * This method is used to set the cash amount.
+     * @param double - the changed value of cash tendered
+     */
+    
+    public void setCash(double mula)
+    {
+        cash = mula;
+    }
+    
+    
+    /**
+     * This method is used to calculate the discount price.
+     * It will use the information provided to calulate the amount
+     * and return the value.
+     * @return - the discounted price of the item
+     */
+    public double getDiscountPrice()
+    {
+        discountPrice = priceOfItem - (priceOfItem * discountPercent);
+        return discountPrice;
+    }
+    
+     /**
+     * This method is used to calculate the final price.
+     * It will use the information provided to calulate the amount
+     * and return the value.
+     * @return - the final price of the item
+     */
+    public double getFinalPrice()
+    {
+        finalPrice = discountPrice + (discountPrice * taxRate);
+        return finalPrice;
+    }
+    
+     /**
+     * This method is used to calculate the change due.
+     * It will use the information provided to calulate the amount
+     * and return the value.
+     * @return - the change due to the customer
+     */
+    public double getChange()
+    {
+        change = cash - finalPrice;
+        return change;
+    } 
+}
